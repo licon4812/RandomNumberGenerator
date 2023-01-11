bfbd3fef90ef64e9decabf3d96f08822770df605
licon4812 <jalickolli@gmail.com>
2023-01-11 11:28:32 +1100

    Created Basic UI components

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace RandomNumberGenerator
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
