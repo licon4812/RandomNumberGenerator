8157a990d2f283f210542c16ab6bda60b02b5d9a
licon4812 <jalickolli@gmail.com>
2023-02-08 10:32:20 +1100

    Created DecisionActivity(android)

--- android/app/src/main/java/com/alickollisofware/randomnumbergenerator/decision/DecisionFragment.kt
+++ android/app/src/main/java/com/alickollisofware/randomnumbergenerator/decision/DecisionFragment.kt
@@ -1,15 +1,13 @@
-package com.alickollisofware.randomnumbergenerator
+package com.alickollisofware.randomnumbergenerator.decision
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alickollisofware.randomnumbergenerator.R
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,16 +15,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DecisionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -50,10 +41,6 @@ class DecisionFragment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             DecisionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
             }
     }
 }
