83781c8138afddec1dd654b8ccd718596eba76b1
licon4812 <jalickolli@gmail.com>
2023-02-08 20:38:34 +1100

    UWP can now randomly pick a decision

--- UWP/DecisionPage.xaml.cs
+++ UWP/DecisionPage.xaml.cs
@@ -17,7 +17,22 @@ namespace RandomNumberGenerator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            //get text from text box with the name DecisionOptions
+            string decisionOptions = DecisionOptions.Text;
+            if (decisionOptions == "")
+            {
+                Decision.Text = "List was Empty";
+            }
+            else
+            {
+                //split the text into an array of strings
+                string[] options = decisionOptions.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                //get a random number between 0 and the number of options
+                Random random = new Random();
+                int randomNumber = random.Next(0, options.Length);
+                //display the random option
+                Decision.Text = options[randomNumber];
+            }
         }
 
         private void NavigationView_ItemInvoked(Windows.UI.Xaml.Controls.NavigationView sender, Windows.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
