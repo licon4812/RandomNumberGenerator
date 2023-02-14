75ac73067ed945f3d0d3091b08f3db0d2ecfcab0
licon4812 <jalickolli@gmail.com>
2023-02-14 10:49:08 +1000

    Theme Switch now works (Android)

--- android/app/src/main/java/com/alickollisofware/randomnumbergenerator/SettingsActivity.kt
+++ android/app/src/main/java/com/alickollisofware/randomnumbergenerator/SettingsActivity.kt
@@ -1,6 +1,4 @@
 package com.alickollisofware.randomnumbergenerator
-
-import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,7 +6,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportFragmentManager.beginTransaction()
-            .replace(R.id.content, SettingsFragment())
+            .replace(android.R.id.content, SettingsFragment())
             .commit()
     }
 }
--- android/app/src/main/java/com/alickollisofware/randomnumbergenerator/SettingsFragment.kt
+++ android/app/src/main/java/com/alickollisofware/randomnumbergenerator/SettingsFragment.kt
@@ -1,14 +1,23 @@
 package com.alickollisofware.randomnumbergenerator
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
 
-    override fun onCreatePreferences(
-        savedInstanceState: Bundle?,
-        rootKey: String?
-    ) {
-        setPreferencesFromResource(R.xml.preferences, rootKey);
+        val switchPreference = findPreference<SwitchPreference>("theme_preference")
+
+        switchPreference?.setOnPreferenceChangeListener { preference, newValue ->
+            if (newValue as Boolean) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            true
+        }
     }
 }
